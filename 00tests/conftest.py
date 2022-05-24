ab81f6a192012e5d76459dc4ac5c0ea88c672191
Mostafa Farrag <moah.farag@gmail.com>
2022-05-25 00:46:56 +0200

    remove tests to fix github ci error

--- 00tests/conftest.py
+++
@@ -1,3 +0,0 @@
-from tests.catchment.conftest import *
-from tests.raster.conftest import *
-from tests.vector.conftest import *
--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,10 @@
+import os
+
 import geopandas as gpd
 
 
 def basin_polygon():
+    print(os.getcwd())
+    print(os.listdir())
+    print(os.listdir("tests/"))
     assert gpd.read_file("tests/basin.geojson")
