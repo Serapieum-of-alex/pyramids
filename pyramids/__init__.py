629df3e59cd567883fd3d840e23350fdd922ab19
Mostafa Farrag <moah.farag@gmail.com>
2022-05-24 23:19:08 +0200

    add tests

---
+++ pyramids/__init__.py
@@ -0,0 +1,14 @@
+"""
+Created on Sat Apr 24 20:57:45 2021
+
+@author: mofarrag
+"""
+
+
+# from .catchment import *
+# from .raster import *
+# from .vector import *
+
+import pyramids.catchment as giscatchment
+import pyramids.raster as raster
+import pyramids.vector as vector
---
+++ tests/conftest.py
@@ -0,0 +1,3 @@
+from tests.catchment.conftest import *
+from tests.raster.conftest import *
+from tests.vector.conftest import *
