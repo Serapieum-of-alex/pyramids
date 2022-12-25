fca7dd172c7f45545d30f3fb764c8867d58d07f1
Mostafa Farrag <moah.farag@gmail.com>
2022-12-25 23:21:38 +0100

    lock numpy to v 1.23.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements_dev = [line.strip() for line in open("requirements-dev.txt").readli
 
 setup(
     name="pyramids-gis",
-    version="0.2.9",
+    version="0.2.10",
     description="GIS utility package",
     author="Mostafa Farrag",
     author_email="moah.farag@gmail.come",
