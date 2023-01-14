38c7cef7ca84758309b07182d2129e4f85085f72
Mostafa Farrag <moah.farag@gmail.com>
2023-01-14 20:40:50 +0100

    add utility module (#32)
    * add utility module
    
    * bump up numpy version
    
    * refactor
    
    * refactor
    
    * bump up versions
    
    * bump up versions
    
    * add test
    
    * refactor code

---
+++ tests/utils/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+@pytest.fixture(scope="module")
+def compressed_raster() -> str:
+    return "tests/data/chirps-v2.0.2009.01.01.tif.gz"
+
+@pytest.fixture(scope="module")
+def uncompressed_output() -> str:
+    return "tests/data/chirps-v2.0.2009.01.01.tif"
---
+++ tests/utils/test_utils.py
@@ -0,0 +1,12 @@
+import os
+from pyramids.utils import extractFromGZ
+
+def test_extractFromGZ(
+        compressed_raster: str,
+        uncompressed_output: str,
+):
+    extractFromGZ(compressed_raster, uncompressed_output, delete = False)
+    assert os.path.exists(uncompressed_output)
+    # deleting the uncompressed file
+    os.remove(uncompressed_output)
+
