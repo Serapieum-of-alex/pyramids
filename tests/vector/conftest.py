c95b57a712cda7cfb8532135bf56e62f57c79ede
Mostafa Farrag <moah.farag@gmail.com>
2023-01-20 23:47:08 +0100

    update tests data

--- tests/vector/conftest.py
+++ tests/vector/conftest.py
@@ -1,11 +1,12 @@
-# import geopandas as gpd
 # import numpy as np
 import pytest
 
 # import datetime as dt
-# from osgeo import ogr
-# from osgeo.ogr import DataSource
-# from osgeo.gdal import Dataset
+from osgeo import ogr
+from osgeo.ogr import DataSource
+import geopandas as gpd
+from geopandas.geodataframe import GeoDataFrame
+
 
 @pytest.fixture(scope="module")
 def create_vector_path() -> str:
@@ -14,3 +15,14 @@ def create_vector_path() -> str:
 @pytest.fixture(scope="module")
 def test_vector_path() -> str:
     return "tests/data/test_vector.geojson"
+
+
+@pytest.fixture(scope="module")
+def data_source(test_vector_path: str) -> DataSource:
+    return ogr.Open(test_vector_path)
+
+
+
+@pytest.fixture(scope="module")
+def gdf(test_vector_path: str) -> GeoDataFrame:
+    return gpd.read_file(test_vector_path)
