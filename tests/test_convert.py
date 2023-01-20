999d6b30d48797f48abff0637898526d5f9f8fd9
Mostafa Farrag <moah.farag@gmail.com>
2023-01-20 19:44:49 +0100

    refactor

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,12 +1,12 @@
 import os
+
 import geopandas as gpd
 from osgeo.gdal import Dataset
+
 from pyramids.convert import Convert
 
-def test_polygonize(
-        test_image: Dataset,
-        polygonized_raster_path: str
-):
+
+def test_polygonize(test_image: Dataset, polygonized_raster_path: str):
     Convert.polygonize(test_image, polygonized_raster_path)
     assert os.path.exists(polygonized_raster_path)
     gdf = gpd.read_file(polygonized_raster_path)
