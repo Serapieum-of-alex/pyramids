cace438820effe92cc160c8ecabf8efbb3ca65cd
Mostafa Farrag <moah.farag@gmail.com>
2023-01-23 01:19:34 +0100

    update necessities

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements_dev = [line.strip() for line in open("requirements-dev.txt").readli
 
 setup(
     name="pyramids-gis",
-    version="0.2.12",
+    version="0.3.0",
     description="GIS utility package",
     author="Mostafa Farrag",
     author_email="moah.farag@gmail.come",
