278ec9526fc6cb148a0349367aa7c9833558c5d3
Mostafa Farrag <moah.farag@gmail.com>
2023-03-05 16:13:35 +0100

    finish create Raster method and test them

--- tests/raster/conftest.py
+++ tests/raster/conftest.py
@@ -10,6 +10,10 @@ from osgeo.gdal import Dataset
 
 
 @pytest.fixture(scope="module")
+def src_path() -> str:
+    return "examples/data/acc4000.tif"
+
+@pytest.fixture(scope="module")
 def src() -> Dataset:
     return gdal.Open("examples/data/acc4000.tif")
 
