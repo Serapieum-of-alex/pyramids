2e4966629c56aefce8d717b1a970963d3207e4d5
Mostafa Farrag <moah.farag@gmail.com>
2023-03-17 07:15:04 +0100

    test read ascii folder

--- tests/raster/test_dataset.py
+++ tests/raster/test_dataset.py
@@ -1,12 +1,12 @@
+from typing import List
 import os
 import numpy as np
-
 from osgeo import gdal
 from pyramids.raster import Raster, Dataset
 
 
 class TestReadDataset:
-    def test_read_all_inside_folder_without_order(
+    def test_read_all_without_order(
         self,
         rasters_folder_path: str,
         rasters_folder_rasters_number: int,
@@ -20,7 +20,7 @@ class TestReadDataset:
         assert dataset.rows == rasters_folder_dim[0]
         assert dataset.columns == rasters_folder_dim[1]
 
-    def test_read_all_inside_folder_with_order(
+    def test_read_all_with_order(
         self,
         rasters_folder_path: str,
         rasters_folder_rasters_number: int,
@@ -57,22 +57,52 @@ class TestReadDataset:
         assert dataset.rows == rasters_folder_dim[0]
         assert dataset.columns == rasters_folder_dim[1]
 
-    def test_from_netcdf(nc_path: str):
-        Dataset.readNC(nc_path, "", separator="_")
+    # def test_from_netcdf(nc_path: str):
+    #     Dataset.readNC(nc_path, "", separator="_")
 
+class TestAscii:
+    def test_read_all_without_order(
+        self,
+        ascii_folder_path: str,
+        rasters_folder_rasters_number: int,
+        rasters_folder_dim: tuple,
+    ):
+        dataset = Dataset.read_separate_files(ascii_folder_path, with_order=False, extension=".asc")
+        assert isinstance(dataset.raster, gdal.Dataset)
+        assert dataset.no_data_value[0] == 2147483648.0
+        assert isinstance(dataset.files, list)
+        assert dataset.time_lenth == rasters_folder_rasters_number
+        assert dataset.rows == rasters_folder_dim[0]
+        assert dataset.columns == rasters_folder_dim[1]
 
-def test_read_array(
-    rasters_folder_path: str,
-    rasters_folder_rasters_number: int,
-    rasters_folder_dim: tuple,
-):
-    dataset = Dataset.read_separate_files(rasters_folder_path, with_order=False)
-    arr = dataset.read_array()
-    assert arr.shape == (
-        rasters_folder_rasters_number,
-        rasters_folder_dim[0],
-        rasters_folder_dim[1],
-    )
+class TestReadArray:
+    def test_geotiff(
+            self,
+            rasters_folder_path: str,
+            rasters_folder_rasters_number: int,
+            rasters_folder_dim: tuple,
+    ):
+        dataset = Dataset.read_separate_files(rasters_folder_path, with_order=False)
+        arr = dataset.read_array()
+        assert arr.shape == (
+            rasters_folder_rasters_number,
+            rasters_folder_dim[0],
+            rasters_folder_dim[1],
+        )
+
+    def test_ascii(
+            self,
+            ascii_folder_path: str,
+            rasters_folder_rasters_number: int,
+            rasters_folder_dim: tuple,
+    ):
+        dataset = Dataset.read_separate_files(ascii_folder_path, with_order=False, extension=".asc")
+        arr = dataset.read_array()
+        assert arr.shape == (
+            rasters_folder_rasters_number,
+            rasters_folder_dim[0],
+            rasters_folder_dim[1],
+        )
 
 
 def test_crop_folder(
@@ -80,5 +110,15 @@ def test_crop_folder(
     crop_aligned_folder_path: str,
     crop_aligned_folder_saveto: str,
 ):
-    Raster.cropAlignedFolder(crop_aligned_folder_path, src, crop_aligned_folder_saveto)
+    Dataset.crop_aligned(crop_aligned_folder_path, src, crop_aligned_folder_saveto)
     assert len(os.listdir(crop_aligned_folder_saveto)) == 3
+
+
+def test_merge(
+    merge_input_raster: List[str],
+    merge_output: str,
+):
+    Dataset.gdal_merge(merge_input_raster, merge_output)
+    assert os.path.exists(merge_output)
+    src = gdal.Open(merge_output)
+    assert src.GetRasterBand(1).GetNoDataValue() == 0
