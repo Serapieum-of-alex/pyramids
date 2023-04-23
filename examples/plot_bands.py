8f962e026fecdea32b4d64149f60f935db288af8
Mostafa Farrag <moah.farag@gmail.com>
2023-04-23 11:51:23 +0200

    plot rgb example

---
+++ examples/plot_bands.py
@@ -0,0 +1,23 @@
+import matplotlib
+
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+from pyramids.dataset import Dataset, Datacube
+
+path = "tests/data/raster-folder/1_MSWEP_1979.01.02.tif"
+#%%
+dataset = Dataset.read_file(path)
+fig, ax = dataset.plot(band=0)
+
+#%%
+rasters_folder_path = "tests/data/raster-folder"
+cube = Datacube.read_separate_files(rasters_folder_path, with_order=False)
+cube.read_dataset()
+# dataset = cube.iloc(0)
+# fig, ax= dataset.plot()
+cube.plot()
+#%% plot rgb
+dataset = Dataset.read_file(
+    "tests/data/geotiff/S2A_MSIL2A_20200215T082021_N0214_R121_T36SXA_20200215T110825_image_0_0.tif"
+)
+fig, ax = dataset.plot()
