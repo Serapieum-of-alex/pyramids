df23684c8afc3083f002cb1efeeb96d1c0227d53
Mostafa Farrag <moah.farag@gmail.com>
2026-03-20 23:00:18 +0100

    add StructuralInfo to the list of exported classes

--- src/pyramids/netcdf/__init__.py
+++ src/pyramids/netcdf/__init__.py
@@ -1,7 +1,9 @@
 """NetCDF subpackage for pyramids."""
 
 from pyramids.netcdf.netcdf import NetCDF
-from pyramids.netcdf.models import NetCDFMetadata, DimensionInfo, ArrayInfo, GroupInfo
+from pyramids.netcdf.models import (
+    NetCDFMetadata, DimensionInfo, ArrayInfo, GroupInfo, StructuralInfo
+)
 from pyramids.netcdf.metadata import get_metadata, to_json, from_json, to_dict
 
 __all__ = [
@@ -10,6 +12,7 @@ __all__ = [
     "DimensionInfo",
     "ArrayInfo",
     "GroupInfo",
+    "StructuralInfo",
     "get_metadata",
     "to_json",
     "from_json",
