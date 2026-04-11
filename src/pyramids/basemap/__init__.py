e1bef39aeeebba1deeaf2b6d10c295aded4a8d05
Mostafa Farrag <moah.farag@gmail.com>
2026-04-12 00:51:58 +0200

    refactor: replace lazy imports with direct imports for add_basemap and get_provider

--- src/pyramids/basemap/__init__.py
+++ src/pyramids/basemap/__init__.py
@@ -6,41 +6,8 @@ optionally warped to the data's CRS using GDAL.
 
 Requires optional dependencies: mercantile, xyzservices, Pillow.
 Install with: ``pyramids-gis[viz]``
-
-Lazy imports are used so that importing this package does not fail
-when the optional dependencies are not installed.
 """
 
-
-def add_basemap(*args, **kwargs):
-    """Add a basemap to a matplotlib Axes.
-
-    Thin wrapper that lazily imports the real implementation from
-    ``pyramids.basemap.basemap`` to avoid import errors when
-    optional dependencies are not installed.
-
-    See Also:
-        pyramids.basemap.basemap.add_basemap: Full implementation
-            with all parameters documented.
-    """
-    from pyramids.basemap.basemap import add_basemap as _impl
-
-    return _impl(*args, **kwargs)
-
-
-def get_provider(*args, **kwargs):
-    """Resolve a tile provider by name.
-
-    Thin wrapper that lazily imports the real implementation from
-    ``pyramids.basemap.basemap``.
-
-    See Also:
-        pyramids.basemap.basemap.get_provider: Full implementation
-            with all parameters documented.
-    """
-    from pyramids.basemap.basemap import get_provider as _impl
-
-    return _impl(*args, **kwargs)
-
+from pyramids.basemap.basemap import add_basemap, get_provider
 
 __all__ = ["add_basemap", "get_provider"]
